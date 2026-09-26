e7d9ec3ce875a24634c75ae9715663170ceb6cf6
indo-mining <rajivranjan4715@gmail.com>
2026-09-27 00:20:31 +0530

    Add asyncHandler middleware for promise handling

---
+++ src/middleware/asyncHandler.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function asyncHandler(fn) {
+  return function asyncHandlerWrapper(req, res, next) {
+    Promise
+      .resolve(fn(req, res, next))
+      .catch(next);
+  };
+}
+
+module.exports = asyncHandler;
